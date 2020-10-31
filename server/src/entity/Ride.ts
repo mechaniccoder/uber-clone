0799fdc6d9c7cfa85e46299ee9ebb10e1c469dc6
mechaniccoder <yuseunghwan94@gmail.com>
2020-10-31 21:28:56 +0900

    Add Ride entity

---
+++ server/src/entity/Ride.ts
@@ -0,0 +1,61 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { rideStatus } from "../types/types";
+
+@Entity()
+class Ride extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: string;
+  @Column({
+    type: "text",
+    enum: ["ACCEPTED", "FINISHED", "CANCELED", "REQUESTED", "ONROUTE"],
+  })
+  status: rideStatus;
+
+  @Column({ type: "text" })
+  pickUpAddress: string;
+
+  @Column({ type: "text" })
+  pickUpTime: string;
+
+  @Column({ type: "double precision", default: 0 })
+  pickUpLat: number;
+
+  @Column({ type: "double precision", default: 0 })
+  pickUpLng: number;
+
+  @Column({ type: "text" })
+  dropOffTime: string;
+
+  @Column({ type: "text" })
+  dropOffAddress: string;
+
+  @Column({ type: "double precision", default: 0 })
+  dropOffLat: number;
+
+  @Column({ type: "double precision", default: 0 })
+  dropOffLng: number;
+
+  @Column({ type: "double precision", default: 0 })
+  price: number;
+
+  @Column({ type: "text" })
+  duration: string;
+
+  @Column({ type: "text" })
+  distance: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+}
+
+export default Ride;
